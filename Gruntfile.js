652a244970deeb0922108be7e678bff898336e60
Steve Blaurock <sblaurock@gmail.com>
2014-07-26 18:59:08 -0400

    Added uglify task to Grunt via RequireJS - now using minified version of "library" code. Added watch task to Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,19 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     version: grunt.file.readJSON('package.json').version,
+    outFile: 'public/js/slooth.min.js',
+    banner: [
+      '/*',
+      ' * Slooth <%= version %>',
+      ' * https://github.com/sblaurock/slooth',
+      ' * @sblaurock - <%= grunt.template.today("yyyy-mm-dd") %> - Licensed MIT',
+      ' */\n\n'
+    ].join('\n'),
 
     requirejs: {
       compile: {
         options: {
+          namespace: 'slooth',
           baseUrl: './',
           name: 'src/initialize',
           paths: {
@@ -18,17 +27,44 @@ module.exports = function(grunt) {
           include: [
             'lib/require'
           ],
-          out: "public/js/slooth.js",
-          preserveLicenseComments: false,
-          optimize: 'none'
+          out: '<%= outFile %>',
+          optimize: 'uglify2'
         }
       }
     },
+
+    concat: {
+      options: {
+        stripBanners: false,
+        banner: '<%= banner %>',
+      },
+      dist: {
+        src: '<%= outFile %>',
+        dest: '<%= outFile %>',
+      }
+    },
+
+    watch: {
+      library: {
+        files: [
+          'Gruntfile.js',
+          'src/*'
+        ],
+        tasks: [
+          'requirejs',
+          'concat'
+        ]
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', [
-    'requirejs'
+    'requirejs',
+    'concat',
+    'watch'
   ]);
 };
